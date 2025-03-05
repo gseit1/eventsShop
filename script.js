504ab9ba61964e49b8da616d6ac41d4398191b2c
gseit1 <115555746+gseit1@users.noreply.github.com>
2025-03-05 23:42:16 +0200

    removed  prevent default

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();  // Αποτροπή της προεπιλεγμένης συμπεριφοράς της φόρμας
-    alert("Η φόρμα σας υποβλήθηκε επιτυχώς!");
-});
