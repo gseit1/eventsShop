357436c92430d8ce11906b32b9398d545feb1c5e
gseit1 <115555746+gseit1@users.noreply.github.com>
2025-03-05 14:22:16 +0200

    1st

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();  // Αποτροπή της προεπιλεγμένης συμπεριφοράς της φόρμας
+    alert("Η φόρμα σας υποβλήθηκε επιτυχώς!");
+});
